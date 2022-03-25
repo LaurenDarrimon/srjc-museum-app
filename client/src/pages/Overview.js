b6413af70e762fb603413aefb23e8b2b5b52da6d
Erin <erinmckaypotter@gmail.com>
2022-03-25 16:36:42 -0700

    styling Overview & Instructions pages, including adding react-bootstrap elements

--- client/src/pages/Overview.js
+++ client/src/pages/Overview.js
@@ -10,26 +10,33 @@ const Overview = () => {
 
   //return JSX
   return (
-    //loading or intro screen and homepage
-    <main className="overview row site-content">
-      <div className="speech-bubble-md col-md-8">
-        <h1>Welcome!</h1>
-        <p>
-          Some text describing what the game is about and how it works. North
-          American / Native American art. Will take you through 4 geographic
-          regions, and their art by looking for artifacts in the
-          museum...etc..etc.
-        </p>
+    //Overview of game
+    <Container>
+      <Row xs={1} className="overview site-content">
+        <Col xs={11} md={{span: 8, offset: 3}} className="speech-bubble speech-bubble-md">
+          <h1 className="display-5">Welcome!</h1>
+          <p>
+            Some text describing what the game is about and how it works. North
+            American / Native American art. Will take you through 4 geographic
+            regions, and their art by looking for artifacts in the
+            museum...etc..etc.
+          </p>
 
-        <h2>Let's test your skills!</h2>
-        <Link to="/instructions" className="big-round-GO-btn">
-          <img src={go} alt="GO!" />
-        </Link>
-      </div>
-      <div className="mascot-container col-md-8">
-        <img src={mascotLarge} alt="mascot animal illustration" />
-      </div>
-    </main>
+          <h2 className="display-5 d-inline-block">Let's test your skills!</h2>
+          <Link to="/instructions" className="big-round-GO-btn float-end">
+            <img className="img-fluid" src={go} alt="GO!" />
+          </Link>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={{span: 6, offset: 1}} className="mascot-container">
+          <img className="img-fluid" src={mascotLarge} alt="mascot animal illustration" />
+        </Col>
+      </Row>
+    </Container>
+      
+ 
   );
 };
 
