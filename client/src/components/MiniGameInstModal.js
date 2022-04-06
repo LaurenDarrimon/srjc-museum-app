dc31f659fe40da623717da4bd07bd9a4a09e56b5
Erin <erinmckaypotter@gmail.com>
2022-04-06 15:27:14 -0700

    create 'Try Again' Modal, style 'Instructions' modal

--- client/src/components/MiniGameInstModal.js
+++ client/src/components/MiniGameInstModal.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 //import { Link } from "react-router-dom";
-import { Button, Modal } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
-
+//images
+import mascotGuideInstructions from "../assets/images/speech-bubble-mascot-instructions.png";
+import go from "../assets/images/go-btn.png"
+import mascotGuide from "../assets/images/speech-bubble-mascot-tip-2.png";
 
 const InstructionModal = () => {
   //full screen modal with instructions for mini-games
@@ -13,39 +16,40 @@ const InstructionModal = () => {
   
     return (
       <>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
-        </Button>
+        <div className='showInstructions-btn'>
+          <img 
+            onClick={handleShow}
+            src={mascotGuide}
+            alt="You've got this!"
+            className="img-fluid"
+          />
+        </div>
   
         <Modal 
           show={show}
           onHide={handleClose}
-          dialogClassName="instructions-modal"
-          aria-labelledby="example-custom-modal-styling-title"
+          dialogClassName="custom-modal"
+          aria-labelledby="instructions-modal-title"
           >
           <Modal.Header closeButton>
-            <Modal.Title>Mini Game Title</Modal.Title>
+            <Modal.Title as="h1"className="instructions-modal-title">Region / Title <br /><span className="instructions-modal-subtitle">Mini-Game</span></Modal.Title>  
           </Modal.Header>
           <Modal.Body className="instructions-modal-body">
             <p>
               Now that you've learned about 
               the people of the American Great Plains, lets put your brain to work!
             </p>
-            <p>
+            <p className='bold-text'>
               What sorts of actions would earn a feather in your ceremonial war bonnet?
             </p>
             <p>
               If a card contains a good deed that would earn a feather, Drag and Drop 
               the card onto the War Bonnet on the right. (Definitely need to workshop the wording)
             </p>
+            <img className='modal-img-mascot-instructions img-fluid' src={mascotGuideInstructions} alt="Click on the Mascot in the lower left corner to see these instructions again" />
             </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
+            <img onClick={handleClose} className='img-fluid' src={go} alt="GO!" />
           </Modal.Footer>
         </Modal>
       </>
