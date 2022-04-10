63914ed30678e6327c393f2d9bbcd4ab3ed4a79a
LaurenDarrimon <laurendarrimon@gmail.com>
2022-04-10 00:10:28 -0700

    add matching game with logic and flip card animations

---
+++ client/src/components/Card.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../assets/images/logo-500.png";
+import classnames from "classnames";
+
+
+const Card = ({ onClick, card, index, isInactive, isFlipped, isDisabled }) => {
+  const handleClick = () => {
+    !isFlipped && !isDisabled && onClick(index);
+  };
+
+  return (
+    <div
+      className={classnames("card", {
+        "is-flipped": isFlipped,
+        "is-inactive": isInactive
+      })}
+      onClick={handleClick}
+    >
+      <div className="card-face card-font-face">
+        <img src={logo} alt="SRJC logo" />
+      </div>
+      <div className="card-face card-back-face">
+        <img src={card.image} alt="artifact" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
