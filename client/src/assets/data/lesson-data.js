997468896410335f14e95023139519ad5c0a24f8
Erin <erinmckaypotter@gmail.com>
2022-04-13 12:42:36 -0700

    add lesson image paths to lesson-data.js

--- client/src/assets/data/lesson-data.js
+++ client/src/assets/data/lesson-data.js
@@ -6,8 +6,8 @@ export const lessonData = [
         p1: "Native Californians lived in an area rich in different types of plants and animals that they could use for food and other supplies.",
         p2: "They ate seeds, roots, deer, elk and sea food. They even ground their own flour from acorns.",
         p3: "Some California tribes that you may already know about are the Pomo, Wappo, and Coast Miwok",
-        imagePath: "",
-        imageDescription: "",
+        imagePath: "../images/lessons/CAlesson1-landscape.jpg",
+        imageDescription: "Scenic view of California oak forest",
     },
     {
         number: 1,
@@ -15,8 +15,8 @@ export const lessonData = [
         p1: "Native Californians were expert basket-makers.",
         p2: "Each family had their own set of baskets that they used for pots, pans and dishes.",
         p3: "The baskets are so sturdy that people would cook in them! Many baskets were also woven tightly enough to hold water.",
-        imagePath: "",
-        imageDescription: "",
+        imagePath: "../images/lessons/CAlesson2-Washoe-Sara-Mayo_and_family.jpg",
+        imageDescription: "Portrait of a Washoe family",
     },
     {
         number: 2,
@@ -24,8 +24,8 @@ export const lessonData = [
         p1: "Baskets were traditionally woven by women.",
         p2: "But one type of basket that men make are the fishing and bird trap baskets.",
         p3: "They would use these to hunt for food.",
-        imagePath: "",
-        imageDescription: "",
+        imagePath: "../images/lessons/CAlesson3-Pomo-basket-weaver-thousand-stick-basket.jpg",
+        imageDescription: "A Pomo basket weaver working on a 'thousand stick basket'",
     },
     //PNW
     {
@@ -34,7 +34,7 @@ export const lessonData = [
         p1: "The Pacific Northwest (a.k.a coastal Oregon, Washington and British Columbia) is full of huge forests, and the climate is mild with lots of rainfall. This is because it is next to the Pacific Ocean.",
         p2: "The native people of this region are known for their woodworking skills, because of the plentiful red cedar trees in the region.",
         p3: "",
-        imagePath: "",
+        imagePath: "../images/lessons/PNWlesson1-western-red-cedar.jpg",
         imageDescription: "",
     },
     {
@@ -43,7 +43,7 @@ export const lessonData = [
         p1: "Cedar trees are very important to Pacific Northwest Coast people.",
         p2: "Families built cedar plank long-houses, where 30 - 40 people lived together",
         p3: "At the front of the house, totem poles were placed to represent the family history and social status of the house members.",
-        imagePath: "",
+        imagePath: "../images/lessons/PNWlesson2-Haida_house_totem_pole_regalia_Haina.jpg",
         imageDescription: "",
     },
     {
@@ -52,7 +52,7 @@ export const lessonData = [
         p1: "People of the Pacific NW lived in smaller groups within their tribes called clans. Each of the clans were associated with animals, such as Bear, Whale, Raven, and Wolf.",
         p2: "Totem poles usually show carvings of these animals, because clans consider these animals as spiritual beings and their direct ancestors.",
         p3: "",
-        imagePath: "",
+        imagePath: "../images/lessons/PNWlesson3-calvinhunt-2003-eagle-chief-and-bear-holding-salmon.jpg",
         imageDescription: "",
     },
     //SW
@@ -62,7 +62,7 @@ export const lessonData = [
         p1: "The Puebloan people of the Southwest live in villages called pueblos in the hot, dry desserts of Arizona, New Mexico and Colorado.",
         p2: "",
         p3: "",
-        imagePath: "",
+        imagePath: "../images/lessons/SWlesson1-Pueblo.jpg",
         imageDescription: "",
     },
     {
@@ -71,7 +71,7 @@ export const lessonData = [
         p1: "They use clay from the soil to make bricks to build their homes and pottery for cooking. ",
         p2: "The pottery is made from long ropes of clay that are pinched together, smoothed, painted and baked in a hot fire to harden.",
         p3: "",
-        imagePath: "",
+        imagePath: "../images/lessons/SWlesson2-black-mesa-nm.jpg",
         imageDescription: "",
     },
     {
@@ -89,16 +89,16 @@ export const lessonData = [
         title: "People of the Great Plains",
         p1: "Imagine living in the midwestern prairie with lots of different kinds of grass growing in all directions.",
         p2: "The Great Plains Indians lived there surrounded by endless rolling hills, few trees and vast herds of huge bison, which they hunted.",
-        p3: "",
+        p3: "../images/lessons/GPlesson3-bison.jpg",
         imagePath: "",
     },
     {
         number: 10,
         title: "People of the Great Plains",
-        p1: "We often think of the Plains Indians as nomadic, which means that they moved around with the seasons instead of building permanent villages. ",
+        p1: "We often think of the Plains Indians as nomadic, which means that they moved around with the seasons instead of building permanent villages.",
         p2: "",
         p3: "",
-        imagePath: "",
+        imagePath: "../images/lessons/GPlesson1-Kiowa-woman-and-children.jpg",
     },
     {
         number: 11,
@@ -106,6 +106,6 @@ export const lessonData = [
         p1: "Eagle feather headdresses were traditionally worn by both warriors and chiefs of the Great Plains tribes.",
         p2: "The feathers hold great spiritual meaning and must be earned before they can be worn.",
         p3: "The feathers are a measure of a person's status, representing acts of bravery, courage, or kindness, and are presented in a public ceremony.",
-        imagePath: "",
+        imagePath: "../images/lessons/GPlesson2-Black_Foot_Standing_Bear_Big_Eagle_Sioux.jpg",
     }
 ]
