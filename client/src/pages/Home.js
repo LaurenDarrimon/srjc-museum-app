a4dc8787d02065a6c8a891332ce56d9c5e8424bc
Erin <erinmckaypotter@gmail.com>
2022-04-18 22:50:50 -0700

    finish styling new alt home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/images/logo-500.png";
-import play from "../assets/images/play-btn.png";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Accordion } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+//images
+import logo from "../assets/images/museum-logo-red-blue-bar.png";
+import logo2 from "../assets/images/logo-500.png";
+import next from "../assets/images/next.png";
+import animalGuide from "../assets/images/coyote.svg"
 
 //this is the loading page
 const Home = () => {
@@ -11,33 +14,47 @@ const Home = () => {
     //loading or intro screen and homepage
     <>
       <Container fluid className="home site-content">
-        <Row className=" text-center">
-          <Col xs={12}>
-            <img
-              src={logo}
-              className="col-md-5 p-4 logo"
-              alt="round SRJC logo showing black on white pottery design"
-            />
+        <Row>
+          <Col xs={{ span: 7, offset: 4 }}>
+            <img className=" pb-2 img-fluid" src={logo} alt="SRJC logo" />
           </Col>
-          <Col xs={12}>
-            <h1 className="display-4">Welcome!</h1>
-            <h2>
-              <span className=" subtitle-home">to the </span>
-              <br />
-              <span className="sub-heading-home display-5">North American Art</span>
-              <br />
-              <span className="display-4">SCAVENGER HUNT</span>
-            </h2>
-         
-            {/*<Link to="/overview">
-              <img src={play} alt="white arrow play button" className="p-4" />
-            </Link>*/}
+          <Col xs={11} md={{ span: 9, offset: 2 }} className="speech-bubble speech-bubble-md">
+            <div className="title-block">
+              <h1>
+                <span className="sub-heading-home display-3">Native American Art</span>
+                <br />
+                <span className="main-heading-home display-2">SCAVENGER HUNT</span>
+              </h1>
+            </div>
+            <div className="main-content">
+              {/*<p className="h2 subtitle-home">
+                Learn about <span className="text-400">Native American Art and Culture</span> while exploring the museum!
+              </p>*/}
+              <Link to="/instructions">
+                <img className="img-fluid next-btn p-5" src={next} alt="next button"/>
+              </Link>
+              <div className="not-onsite-info">
+                <Accordion defaultActiveKey="1" flush>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>
+                      If you are not at the museum, you can still play!
+                      <br />
+                      <br />
+                      Click Here to Learn How.
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      There are <span className="text-400">4 games</span> you can play without any help from the museum's exhibits. Access them through the <span className="text-400">"Mini-Games" link</span> in the menu.
+
+                      You can still try your best on the scavenger hunt if you would like! You wont have access to clues in the museum, but there is no penalty for guessing.
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </div>
+            </div>
+
           </Col>
+          <img className="main-guide-img" src={animalGuide} alt="cute blue coyote" />
         </Row>
-      </Container>
-      <Container fluid className="footer"> 
-          <br></br>
-          <br></br>
       </Container>
     </>
   );
