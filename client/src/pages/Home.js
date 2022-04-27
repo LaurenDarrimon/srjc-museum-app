10f5c8069ec86a80a44feac67a2dd764b5fd5aaf
LaurenDarrimon <laurendarrimon@gmail.com>
2022-04-26 21:49:19 -0700

    restructure homepage so that next button works on all device widths

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //images
 import logo from "../assets/images/museum-logo-red-blue-bar.png";
 import next from "../assets/images/next.png";
-import animalGuide from "../assets/images/coyote.svg"
+import animalGuide from "../assets/images/coyote.svg";
 
 //this is the loading page
 const Home = () => {
@@ -17,17 +17,38 @@ const Home = () => {
           <Col xs={{ span: 7, offset: 4 }}>
             <img className=" pb-3 img-fluid" src={logo} alt="SRJC logo" />
           </Col>
-          <Col xs={11} md={{ span: 9, offset: 2 }} className="speech-bubble text-center">
+          <Col
+            xs={11}
+            md={{ span: 9, offset: 2 }}
+            className="speech-bubble text-center"
+          >
             <h1 className="d-inline-block mx-auto ">
-              <span className="sub-heading-home display-3">Native American Art</span>
+              <span className="sub-heading-home display-3">
+                Native American Art
+              </span>
               <br />
-              <span className="main-heading-home display-2">SCAVENGER HUNT</span>
+              <span className="main-heading-home display-2">
+                SCAVENGER HUNT
+              </span>
             </h1>
             <p className="h3 subtitle-home">
-              Learn about <span className="text-400">Native American Art and Culture </span>while exploring the museum!
+              Learn about{" "}
+              <span className="text-400">Native American Art and Culture </span>
+              while exploring the museum!
             </p>
-            <Link to="/instructions" style={{zIndex: 100}}>
-              <img className="img-fluid next-btn py-5" src={next} alt="next button" />
+          </Col>
+          <Col
+            xs={11}
+            md={{ span: 9, offset: 2 }}
+            className="speech-bubble text-end"
+          >
+            <Link to="/instructions">
+              <img
+                className="img-fluid next-btn py-5 "
+                src={next}
+                alt="next button"
+                style={{ textAlign: "right" }}
+              />
             </Link>
             <div className="bottom-section">
               <div className="not-onsite-info">
@@ -40,18 +61,32 @@ const Home = () => {
                       Click Here to Learn How.
                     </Accordion.Header>
                     <Accordion.Body>
-                       <span className="text-400">There are 4 games you can play without any help from the museum's exhibits.</span> Access them through the <span className="text-400">"Mini-Games" link</span> in the menu.
+                      <span className="text-400">
+                        There are 4 games you can play without any help from the
+                        museum's exhibits.
+                      </span>{" "}
+                      Access them through the{" "}
+                      <span className="text-400">"Mini-Games" link</span> in the
+                      menu.
                       <br />
                       <br />
-                      <span className="text-400">You can still try your best on the scavenger hunt if you would like!</span> You wont have access to clues in the museum, but there is no penalty for guessing.
+                      <span className="text-400">
+                        You can still try your best on the scavenger hunt if you
+                        would like!
+                      </span>{" "}
+                      You wont have access to clues in the museum, but there is
+                      no penalty for guessing.
                     </Accordion.Body>
                   </Accordion.Item>
                 </Accordion>
               </div>
             </div>
-
           </Col>
-          <img className="main-guide-img" src={animalGuide} alt="cute blue coyote" />
+          <img
+            className="main-guide-img"
+            src={animalGuide}
+            alt="cute blue coyote"
+          />
         </Row>
       </Container>
     </>
