d9892cf4655b5001fa897a878cef8fd4de00147b
Erin <erinmckaypotter@gmail.com>
2022-05-08 17:50:18 -0700

    add pottery pattern key images & fix wording

--- client/src/assets/data/southwest-data.js
+++ client/src/assets/data/southwest-data.js
@@ -1,53 +1,118 @@
 export const potteryData = [
   {
     number: 1,
-    title: "Wing1",
-    type: "wing",
-    image: "wing1.png",
-    description: "Patterns with birds and feathers may symbolize prayer."
+    title: "Bird",
+    image: "wing1.svg",
+    description: "Prayer"
   },
   {
     number: 2,
-    title: "Wing2",
-    type: "wing",
-    image: "wing2.png",
-    description: "Birds and feathers can mean prayer."
+    title: "Bird",
+    image: "wing2.svg",
+    description: "Prayer"
   },
   {
     number: 3,
-    title: "Wing3",
-    type: "wing",
-    image: "wing3.png",
-    description: "Patterns with birds and feathers can be prayer symbols."
+    title: "Wing",
+    image: "wing3.svg",
+    description: "Prayer"
   },
   {
     number: 4,
-    title: "Wing4",
-    type: "wing",
-    image: "wings4.png",
-    description: "Birds and feathers are often prayer symbols."
+    title: "Fird",
+    image: "wing4.svg",
+    description: "Prayer"
   },
   {
     number: 5,
-    title: "Wing5",
-    type: "wing",
-    image: "wing5.png",
-    description: "Patterns with birds and feathers are often prayer symbols."
+    title: "Feather",
+    image: "wing5.svg",
+    description: "Prayer"
   },
   {
     number: 6,
-    title: "Rain1",
-    type: "rain",
-    image: "rain1.png",
-    description: "Zigzag lines are often associated with lightning and snakes."
+    title: "Feather",
+    image: "wing6.svg",
+    description: "Prayer"
   },
   {
     number: 7,
-    title: "Rain2",
-    type: "rain",
-    image: "rain2.png",
-    description: "Snakes are associates with water."
+    title: "Diagonal Lines",
+    image: "diagonal-lines-rain.svg",
+    description: "Water"
   },
+  {
+    number: 9,
+    title: "Butterfly",
+    image: "fertility1.svg",
+    description: "Fertility"
+  },
+  {
+    number: 10,
+    title: "Tadpole",
+    image: "fertility2.svg",
+    description: "Fertility"
+  },
+  {
+    number: 11,
+    title: "Turtle",
+    image: "fertility3.svg",
+    description: "Fertility"
+  },
+  {
+    number: 12,
+    title: "The Heartline",
+    image: "heartline1.svg",
+    description: "Pathway of powerful Breath ~ Lungs to Mouth"
+  },
+  {
+    number: 13,
+    title: "The Heartline",
+    image: "heartline2.svg",
+    description: "Pathway of powerful Breath ~ Lungs to Mouth"
+  },
+  {
+    number: 14,
+    title: "Kiva Steps",
+    image: "kiva-steps.svg",
+    description: "Sacred space"
+  },
+  {
+    number: 15,
+    title: "Lightning",
+    image: "lighting.svg",
+    description: "lightning or snakes ~ snakes symbolize water"
+  },
+  {
+    number: 16,
+    title: "Rain",
+    image: "rain1.svg",
+    description: "Rain, Clouds & Lightning"
+  },
+  {
+    number: 17,
+    title: "Rain",
+    image: "rain2.svg",
+    description: "Rain & Clouds"
+  },
+  {
+    number: 18,
+    title: "Rain",
+    image: "rain3.svg",
+    description: "Rain & Clouds"
+  },
+  {
+    number: 19,
+    title: "Rainbow",
+    image: "rainbow.svg",
+    description: "Symbolic of Rain"
+  },
+  {
+    number: 20,
+    title: "Snake",
+    image: "snake.svg",
+    description: "lightning or snakes ~ snakes symbolize water"
+  }
 
 ];
 
